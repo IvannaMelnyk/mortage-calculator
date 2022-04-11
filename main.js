ad5ed9352a6c5a4846c5ca834b91f4d175efe99f
IvannaMelnyk <givmiller@gmail.com>
2022-04-11 11:58:47 +0300

    calculator

--- main.js
+++ main.js
@@ -58,5 +58,40 @@ const takeActiveBank = currentActive => {
   //   console.log(dataAttrValue);
   const currentBank = banks.find(bank => bank.name === dataAttrValue);
   currentPrecent = currentBank.precents;
-  console.log(currentPrecent);
+  calculation(totalCost.value, anInitialFee.value, creditTerm.value);
+};
+for (let input of inputsRange) {
+  input.addEventListener('input', () => {
+    assignValue();
+    calculation(totalCost.value, anInitialFee.value, creditTerm.value);
+  });
+}
+const calculation = (totalCost = 0, anInitialFee = 100000, creditTerm = 1) => {
+  /*ЕП -ежемесячный платеж
+РК-размер кредита
+ПС-процентная ставка
+КМ-количество месяцев
+ЕП=(РК+(РК/100)*ПС/12)*КМ)/КМ
+
+
+*/
+  let monthlyPayment;
+  let lounAmount = totalCost - anInitialFee; //размер кредида
+  let interestRate = currentPrecent; // процентная ставка
+  let numberOfYears = creditTerm; //количество лет
+  let numberOfMonths = 12 * numberOfYears; //количество месяцев
+
+  monthlyPayment =
+    (lounAmount + (((lounAmount / 100) * interestRate) / 12) * numberOfMonths) /
+    numberOfMonths;
+  const monthlyPaymentArounded = Math.round(monthlyPayment);
+  if (monthlyPaymentArounded < 0) {
+    return false;
+  } else {
+    totalAmountOfCredit.innerHTML = `${lounAmount} $`;
+    totalMonthlyPayment.innerHTML = `${monthlyPaymentArounded} $`;
+    totalRecommendedIncome.innerHTML = `${
+      monthlyPaymentArounded + (monthlyPaymentArounded / 100) * 35
+    } $`;
+  }
 };
